12eadbdee503e5bf744d7eae0dae3a57f2ff5d07
KimKimJungyeon <39543001+KimKimJungyeon@users.noreply.github.com>
2020-11-13 23:50:55 +0900

    4'th Day Commit

--- Report1/DBSearch.py
+++ Report1/DBSearch.py
@@ -1,5 +1,8 @@
 import pymysql
 import sys, datetime
+import xml.etree.ElementTree as ET
+import json
+import csv
 
 #DB 접근
 class DB_Utils:
@@ -34,14 +37,18 @@ class DB_Utils:
 class DB_Query:
     # 모든 검색문은 여기에 각각 하나의 메소드로 정의
 
-    # 팀 이름 검색
-    def selectTeam(self, value):
+    #플레이어 검색
+    def selectPLTeamName(self, value):
         if value == '없음':
-            sql = "SELECT * FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
-                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE TEAM_NAME= NULL "
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  " FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE TEAM_NAME IS NULL "
             params = ()
         else:
-            sql = "SELECT * FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  " FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
                   "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE TEAM_NAME= %s "
             params = (value)  # SQL문의 실제 파라미터 값의 튜플
 
@@ -49,23 +56,119 @@ class DB_Query:
         tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
         return tuples
 
-    def selectTeamName(self):
-        sql = "SELECT DISTINCT TEAM_NAME FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
-              "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID "
-        params = ()
+    def selectPLPosition(self, value):
+        if value == '미정':
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                      "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE POSITION IS NULL "
+            params = ()
+        else:
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE POSITION= %s "
+            params = (value)  # SQL문의 실제 파라미터 값의 튜플
 
         util = DB_Utils()
         tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
         return tuples
 
-    #선수 검색
-    def selectPlayer(self, value):
-        if value == '없음':
-            sql = "SELECT * FROM player WHERE position IS NULL"
+    def selectPLCountry(self, value):
+        if value == '대한민국':
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                      "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE NATION IS NULL "
             params = ()
         else:
-            sql = "SELECT * FROM player WHERE position = %s"
-            params = (value)         # SQL문의 실제 파라미터 값의 튜플
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE NATION= %s "
+            params = (value)  # SQL문의 실제 파라미터 값의 튜플
+
+        util = DB_Utils()
+        tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
+        return tuples
+
+    def selectPLUpHeight(self, value):
+        if value == '미측정':
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                      "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE HEIGHT IS NULL "
+            params = ()
+        else:
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE HEIGHT>= %s "
+            params = (value)  # SQL문의 실제 파라미터 값의 튜플
+
+        util = DB_Utils()
+        tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
+        return tuples
+
+    def selectPLDnHeight(self, value):
+        if value == '미측정':
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                      "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE HEIGHT IS NULL "
+            params = ()
+        else:
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE HEIGHT<= %s "
+            params = (value)  # SQL문의 실제 파라미터 값의 튜플
+
+        util = DB_Utils()
+        tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
+        return tuples
+
+    def selectPLUpWeight(self, value):
+        if value == '미측정':
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE WEIGHT IS NULL "
+            params = ()
+        else:
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE WEIGHT>= %s "
+            params = (value)  # SQL문의 실제 파라미터 값의 튜플
+
+        util = DB_Utils()
+        tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
+        return tuples
+
+    def selectPLDnWeight(self, value):
+        if value == '미측정':
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE WEIGHT IS NULL "
+            params = ()
+        else:
+            sql = "SELECT PLAYER_ID,PLAYER_NAME,Pl.TEAM_ID TEAM_ID,E_PLAYER_NAME," \
+                  "NICKNAME,JOIN_YYYY,POSITION,BACK_NO,NATION,BIRTH_DATE,SOLAR,HEIGHT,WEIGHT,TEAM_NAME" \
+                  "FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+                  "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID WHERE WEIGHT<= %s "
+            params = (value)  # SQL문의 실제 파라미터 값의 튜플
+
+        util = DB_Utils()
+        tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
+        return tuples
+
+    #팀 이름 검색
+    def selectTeamName(self):
+        sql = "SELECT DISTINCT TEAM_NAME FROM player PL LEFT JOIN(SELECT TEAM_NAME,TEAM_ID " \
+              "FROM team)AS t ON t.TEAM_ID=PL.TEAM_ID "
+        params = ()
 
         util = DB_Utils()
         tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
@@ -81,8 +184,8 @@ class DB_Query:
         return tuples
 
     #출신국 검색
-    def selectPlayerPosition(self):
-        sql = "SELECT DISTINCT position FROM player"
+    def selectPlayerCountry(self):
+        sql = "SELECT DISTINCT NATION FROM player"
         params = ()
 
         util = DB_Utils()
@@ -90,8 +193,8 @@ class DB_Query:
         return tuples
 
     #키 검색
-    def selectPlayerPosition(self):
-        sql = "SELECT DISTINCT position FROM player"
+    def selectPlayerHeight(self):
+        sql = "SELECT DISTINCT HEIGHT FROM player"
         params = ()
 
         util = DB_Utils()
@@ -99,19 +202,69 @@ class DB_Query:
         return tuples
 
     #몸무게 검색
-    def selectPlayerPosition(self):
-        sql = "SELECT DISTINCT position FROM player"
+    def selectPlayerWeight(self):
+        sql = "SELECT DISTINCT WEIGHT FROM player"
         params = ()
 
         util = DB_Utils()
         tuples = util.queryExecutor(db="kleague", sql=sql, params=params)
         return tuples
 
-class DB_Update:
-    # 모든 갱신문은 여기에 각각 하나의 메소드로 정의
-    def insertPlayer(self, player_id, player_name, team_id, position):
-        sql = "INSERT INTO player (player_id, player_name, team_id, position) VALUES (%s, %s, %s, %s)"
-        params = (player_id, player_name, team_id, position)
+class FileOutput:
+    def readDB_writeCSV(self, players):
 
-        util = DB_Utils()
-        util.updateExecutor(db="kleague", sql=sql, params=params)
+        # CSV 화일을 쓰기 모드로 생성
+        with open('player.csv', 'w', encoding='utf-8', newline='') as f:
+            wr = csv.writer(f)
+            columnNames = list(players[0].keys())
+            wr.writerow(columnNames)
+
+            # 테이블 내용을 출력
+            for rowIDX in range(len(players)):
+                row = list(players[rowIDX].values())
+                wr.writerow(row)
+                # 2007001,정병지,K03,"JEONG, BYUNGJI",,2011,GK,1,,1980-08-04,1,184,77
+                # 날짜 변환 기능을 csv 패키지에서 제공함.
+
+
+    def readDB_writeJSON(self, players):
+        for player in players:
+            for k, v in player.items():
+                if isinstance(v, datetime.date):
+                    player[k] = v.strftime('%Y-%m-%d')  # 키가 k인 item의 값 v를 수정
+        newDict = dict(Player= players)
+        # JSON 화일에 쓰기
+        # dump()에 의해 모든 작은 따옴표('')는 큰 따옴표("")로 변환됨
+        with open('player.json', 'w', encoding='utf-8') as f:
+            json.dump(newDict, f, ensure_ascii=False)
+
+    def readDB_writeXML(self, players):
+
+        # 애트리뷰트 BIRTH_DATE의 값을 MySQL datetime 타입에서 스트링으로 변환함. (CSV에서는 패키지가 변환함.)
+        for player in players:
+            for k, v in player.items():
+                if isinstance(v, datetime.date):
+                    player[k] = v.strftime('%Y-%m-%d')  # 키가 k인 item의 값 v를 수정
+
+        newDict = dict(Player = players)
+
+        # XDM 트리 생성
+        tableName = list(newDict.keys())[0]
+        tableRows = list(newDict.values())[0]
+
+        rootElement = ET.Element('Table')
+        rootElement.attrib['name'] = tableName
+
+        for row in tableRows:
+            rowElement = ET.Element('Row')
+            rootElement.append(rowElement)
+
+            for columnName in list(row.keys()):
+                if row[columnName] == None:  # NICKNAME, JOIN_YYYY, NATION 처리
+                    rowElement.attrib[columnName] = ''
+                else:
+                    rowElement.attrib[columnName] = row[columnName]
+                if type(row[columnName]) == int:  # BACK_NO, HEIGHT, WEIGHT 처리
+                    rowElement.attrib[columnName] = str(row[columnName])
+
+        ET.ElementTree(rootElement).write('player.xml', encoding='utf-8', xml_declaration=True)
--- Report1/MainGUI.py
+++ Report1/MainGUI.py
@@ -9,9 +9,16 @@ class MainWindow(QWidget):
         super().__init__()
         self.setupUI()
 
+
     def setupUI(self):
         self.setWindowTitle("Report")
         self.setGeometry(500, 100, 700, 800)  # 위치, 크기 세팅
+        self.TeamnameValue = '사용안함'
+        self.PositionValue = '사용안함'
+        self.WeightValue = '사용안함'
+        self.HeightValue = '사용안함'
+        self.CountryValue = '사용안함'
+        self.Warning = QMessageBox
 
         # 기본적으로 필요한 모든 위젯 생성
 
@@ -52,13 +59,14 @@ class MainWindow(QWidget):
 
         # 푸시버튼 모음
         self.SaveBtn = QPushButton("저장")  # 저장 버튼
-        #self.SaveBtn.clicked.connect(self.pushButton_Clicked)
 
         # 테이블위젯 설정
         self.tableWidget = QTableWidget(self)   # QTableWidget 객체 생성
         #self.tableWidget.resize(1000, 500)
 
         # 레이아웃 생성 ====================================================
+
+        #선수검색 부분 세팅 ======================================
         self.GroupBox1 = QGroupBox("선수검색") # 선수검색들 묶어둘 녀석
         self.BigLayout = QVBoxLayout(self)
         self.PlLayout1 = QHBoxLayout(self) #선수검색 첫번째줄(팀명~출신국 까지)
@@ -77,6 +85,7 @@ class MainWindow(QWidget):
         self.HgtLayout.addWidget(self.HeightUpBtn)
         self.HgtLayout.addWidget(self.HeightDnBtn)
         self.HgtGroupBox.setLayout(self.HgtLayout)
+        self.HeightUpBtn.setChecked(True)
 
         self.WgtGroupBox= QGroupBox() #몸무게 이상, 이하 체크때문에 나눔
         self.WgtLayout = QHBoxLayout(self)#몸무게 세팅
@@ -85,6 +94,7 @@ class MainWindow(QWidget):
         self.WgtLayout.addWidget(self.WeightUpBtn)
         self.WgtLayout.addWidget(self.WeightDnBtn)
         self.WgtGroupBox.setLayout(self.WgtLayout)
+        self.WeightUpBtn.setChecked(True)
 
         self.PlLayout2.addWidget(self.HgtGroupBox)
         self.PlLayout2.addWidget(self.WgtGroupBox)
@@ -110,17 +120,20 @@ class MainWindow(QWidget):
         self.PLLayout = QHBoxLayout(self)
         self.PLLayout.addWidget(self.GroupBox1)
 
+        #검색된 선수들 출력될 부분 ============================
         self.TableLayout = QHBoxLayout(self) #결과창 나올부분
         self.TableLayout.addWidget(self.tableWidget)
 
         self.GroupBox2= QGroupBox("파일 출력")
 
+        #파일 출력파트 세팅 ===============================
         self.FlLayout1= QHBoxLayout(self)
         self.FlLayout1.addWidget(self.CsvRdbtn)
         self.FlLayout1.addWidget(self.JsonRdbtn)
         self.FlLayout1.addWidget(self.XmlRdbtn)
         self.FlBtnLayout= QHBoxLayout(self)
         self.FlBtnLayout.addWidget(self.SaveBtn)
+        self.CsvRdbtn.setChecked(True)
 
         self.FLlayer= QHBoxLayout(self)
         self.FLlayer.addLayout(self.FlLayout1)
@@ -136,14 +149,111 @@ class MainWindow(QWidget):
 
         self.setLayout(self.BigLayout)
 
+        #팀명 콤보박스 내용 추가
+        query = DBSearch.DB_Query()
 
-    """
-    def comboBox_Activated(self):
-        self.positionValue = self.comboBox.currentText()  # positionValue를 통해 선택한 포지션 값을 전달
-    def pushButton_Clicked(self):
+        rows = query.selectTeamName()
+        columnName = list(rows[0].keys())[0]
+        self.TeamNameitems = ['없음' if row[columnName] == None else row[columnName] for row in rows]
+        self.TeamNameitems.sort()
+        self.TeamNameitems.insert(0,'사용안함')
+        self.TeamnameCbbox.addItems(self.TeamNameitems)
+        self.TeamnameCbbox.activated.connect(self.TeamnameCbbox_Activated)
+
+        #포지션 콤보박스 내용추가
+        rows = query.selectPlayerPosition()
+        columnName = list(rows[0].keys())[0]
+        self.Positionitems = ['미정' if row[columnName] == None else row[columnName] for row in rows]
+        self.Positionitems.sort()
+        self.Positionitems.insert(0, '사용안함')
+        self.PositionCbbox.addItems(self.Positionitems)
+        self.PositionCbbox.activated.connect(self.PositionCbbox_Activated)
+
+        #출신국 콤보박스 내용추가
+        rows = query.selectPlayerCountry()
+        columnName = list(rows[0].keys())[0]
+        self.Countryitems = ['대한민국' if row[columnName] == None else row[columnName] for row in rows]
+        self.Countryitems.sort()
+        self.Countryitems.insert(0, '사용안함')
+        self.CountryCbbox.addItems(self.Countryitems)
+        self.CountryCbbox.activated.connect(self.CountryCbbox_Activated)
+
+        # 키 콤보박스 내용추가
+        rows = query.selectPlayerHeight()
+        columnName = list(rows[0].keys())[0]
+        self.Heightitems = ['미측정' if row[columnName] == None else str(row[columnName]) for row in rows]
+        self.Heightitems.sort()
+        self.Heightitems.insert(0, '사용안함')
+        self.HeightCbbox.addItems(self.Heightitems)
+        self.HeightCbbox.activated.connect(self.HeightCbbox_Activated)
+
+        # 몸무게 콤보박스 내용추가
+        rows = query.selectPlayerWeight()
+        columnName = list(rows[0].keys())[0]
+        self.Weightitems = ['미측정' if row[columnName] == None else str(row[columnName]) for row in rows]
+        self.Weightitems.sort()
+        self.Weightitems.insert(0, '사용안함')
+        self.WeightCbbox.addItems(self.Weightitems)
+        self.WeightCbbox.activated.connect(self.WeightCbbox_Activated)
+        #외형 구현 완료
+        self.searchBtn.clicked.connect(self.SearchButton_Clicked)
+        self.ResetBtn.clicked.connect(self.ResetButton_Clicked)
+        self.SaveBtn.clicked.connect(self.SaveBtn_Clicked)
+
+#==================외형, 클릭 이벤트 정리========================================
+    def TeamnameCbbox_Activated(self):
+        self.TeamnameValue = self.TeamnameCbbox.currentText()  # TeamNameValue를 통해 선택한 팀명 값을 전달
+
+    def PositionCbbox_Activated(self):
+        self.PositionValue = self.PositionCbbox.currentText()  # PositionValue를 통해 선택한 포지션 값을 전달
+
+    def CountryCbbox_Activated(self):
+        self.CountryValue = self.CountryCbbox.currentText() #CountryValue를 통해 선택한 출신국 값을 전달
+
+    def HeightCbbox_Activated(self):
+        self.HeightValue = self.HeightCbbox.currentText() #HeightValue를 통해 선택한 키 값을 전달
+
+    def WeightCbbox_Activated(self):
+        self.WeightValue = self.WeightCbbox.currentText() #WeightValue를 통해 선택한 몸무게 값을 전달
+
+    def ResetButton_Clicked(self):
+        self.HeightUpBtn.setChecked(True)
+        self.WeightUpBtn.setChecked(True)
+        self.TeamnameCbbox.clear()
+        self.TeamnameCbbox.addItems(self.TeamNameitems)
+        self.PositionCbbox.clear()
+        self.PositionCbbox.addItems(self.Positionitems)
+        self.CountryCbbox.clear()
+        self.CountryCbbox.addItems(self.Countryitems)
+        self.HeightCbbox.clear()
+        self.HeightCbbox.addItems(self.Heightitems)
+        self.WeightCbbox.clear()
+        self.WeightCbbox.addItems(self.Weightitems)
+        self.tableWidget.clearContents()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)
+
+    def SearchButton_Clicked(self):
         # DB 검색문 실행
-        query = DB_Query()
-        players = query.selectPlayer(self.positionValue)
+        query = DBSearch.DB_Query()
+        if self.TeamnameValue != '사용안함':
+            players = query.selectPLTeamName(self.TeamnameValue)
+        elif self.PositionValue != '사용안함':
+            players= query.selectPLPosition(self.PositionValue)
+        elif self.CountryValue != '사용안함':
+            players= query.selectPLCountry(self.CountryValue)
+        elif self.HeightValue != '사용안함' and self.HeightUpBtn.isChecked(): #이상으로 검색했는가
+            players= query.selectPLUpHeight(self.HeightValue)
+        elif self.HeightValue != '사용안함' and self.HeightDnBtn.isChecked(): #이상으로 검색했는가
+            players= query.selectPLDnHeight(self.HeightValue)
+        elif self.WeightValue != '사용안함' and self.WeightUpBtn.isChecked():
+            players= query.selectPLUpWeight(self.WeightValue)
+        elif self.WeightValue != '사용안함' and self.WeightDnBtn.isChecked():
+            players= query.selectPLDnWeight(self.WeightValue)
+        else:
+            self.Warning.question(self, 'Warning', "검색할 것을 선택해주세요.", QMessageBox.Yes)
+            return
+
         self.tableWidget.clearContents()
         self.tableWidget.setRowCount(len(players))
         self.tableWidget.setColumnCount(len(players[0]))
@@ -154,15 +264,48 @@ class MainWindow(QWidget):
             player = players[rowIDX]
             for k, v in player.items():
                 columnIDX = columnNames.index(k)
-                if v == None:           # 파이썬이 DB의 널값을 None으로 변환함.
-                    continue            # QTableWidgetItem 객체를 생성하지 않음
-                elif isinstance(v, datetime.date):      # QTableWidgetItem 객체 생성
+                if v == None:  # 파이썬이 DB의 널값을 None으로 변환함.
+                    continue  # QTableWidgetItem 객체를 생성하지 않음
+                elif isinstance(v, datetime.date):  # QTableWidgetItem 객체 생성
                     item = QTableWidgetItem(v.strftime('%Y-%m-%d'))
                 else:
                     item = QTableWidgetItem(str(v))
                 self.tableWidget.setItem(rowIDX, columnIDX, item)
         self.tableWidget.resizeColumnsToContents()
-        self.tableWidget.resizeRowsToContents()"""
+        self.tableWidget.resizeRowsToContents()
+
+    def SaveBtn_Clicked(self):
+        #뭘 검색했는가
+        query = DBSearch.DB_Query()
+        if self.TeamnameValue != '사용안함':
+            players = query.selectPLTeamName(self.TeamnameValue)
+        elif self.PositionValue != '사용안함':
+            players = query.selectPLPosition(self.PositionValue)
+        elif self.CountryValue != '사용안함':
+            players = query.selectPLCountry(self.CountryValue)
+        elif self.HeightValue != '사용안함' and self.HeightUpBtn.isChecked():  # 이상으로 검색했는가
+            players = query.selectPLUpHeight(self.HeightValue)
+        elif self.HeightValue != '사용안함' and self.HeightDnBtn.isChecked():  # 이상으로 검색했는가
+            players = query.selectPLDnHeight(self.HeightValue)
+        elif self.WeightValue != '사용안함' and self.WeightUpBtn.isChecked():
+            players = query.selectPLUpWeight(self.WeightValue)
+        elif self.WeightValue != '사용안함' and self.WeightDnBtn.isChecked():
+            players = query.selectPLDnWeight(self.WeightValue)
+        else:
+            self.Warning.question(self, 'Warning', "검색할 것을 선택해주세요.", QMessageBox.Yes)
+            return
+
+        file = DBSearch.FileOutput()
+
+        if self.CsvRdbtn.isChecked():
+            file.readDB_writeCSV(players)
+        elif self.JsonRdbtn.isChecked():
+            file.readDB_writeJSON(players)
+        elif self.XmlRdbtn.isChecked():
+            file.readDB_writeXML(players)
+        else:
+            self.Warning.question(self, 'Warning', "출력방식을 선택해주세요.", QMessageBox.Yes)
+            return
 
 #########################################
 
